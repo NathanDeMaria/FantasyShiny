84db29b52263827d5fda254be03ac4ea4b004597
NathanDeMaria <naterstd@gmail.com>
2014-09-21 15:58:01 -0500

    Added labels inside tabs and deploy to shinyapps

--- app/ui.R
+++ app/ui.R
@@ -7,12 +7,14 @@ shinyUI(fluidPage(
 	tabsetPanel(id = 'tabs', 
 							tabPanel('Players', 
 											 tags$div(
+											 	tags$h3('Select players to compare'),
 											 	selectInput('player1', label = 'Player 1', 
 											 							choices = player_options),
 											 	selectInput('player2', label = 'Player 2', 
 											 							choices = player_options),
 											 	style='height:400px')),
-							tabPanel('Weekly', ggvisOutput('plot')),
-							tabPanel('Total', ggvisOutput('total_plot')),
-							tabPanel('t.test', htmlOutput('ttest')))
+							tabPanel('Weekly', tags$h3('Points per week'), ggvisOutput('plot')),
+							tabPanel('Total', tags$h3('Total points'), ggvisOutput('total_plot')),
+							tabPanel('t.test', tags$h3('t.test comparing point distributions'), 
+											 htmlOutput('ttest')))
 ))
